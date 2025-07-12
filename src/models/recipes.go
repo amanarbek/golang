3394fd4b9d1bbbcc30dd164a7b939670124a81e7
Manarbek Abrashimov <avmanarbek@gmail.com>
2025-07-12 23:46:58 +0500

    Добавил метод сравнения ингредиентов

--- src/models/recipes.go
+++ src/models/recipes.go
@@ -3,6 +3,7 @@ package models
 import (
 	"encoding/xml"
 	"fmt"
+	"strings"
 )
 
 type Ingredient struct {
@@ -58,5 +59,42 @@ func (r Recipes) CompareRecipes(other Recipes) {
 }
 
 func (c Cake) compareIngredients(other Cake) {
+	ings := make(map[string]Ingredient)
+	commonIngs := make(map[string]Ingredient)
 
+	for _, ing := range c.Ingredients {
+		ings[strings.ToLower(ing.Name)] = ing
+	}
+
+	// Сравнивает ингредиенты
+	for _, ing := range other.Ingredients {
+		keyToLow := strings.ToLower(ing.Name)
+		if _, ok := ings[keyToLow]; !ok {
+			fmt.Printf("ADDED ingredient \"%s\" for cake  \"%s\"\n", ing.Name, other.Name)
+		} else {
+			// Добавить общий ингредиент
+			commonIngs[keyToLow] = ing
+		}
+		if ings[keyToLow].Count != ing.Count {
+			fmt.Printf("CHANGED unit count for ingredient \"%s\" for cake  \"%s\" - \"%s\" instead of \"%s\"\n",
+				ing.Name, other.Name, ing.Count, ings[keyToLow].Count)
+		}
+		// Единицы измерения могут быть опущены. Проверка на пустую строку обязательно
+		if ings[keyToLow].Unit != ing.Unit {
+			if ings[keyToLow].Unit == "" {
+				fmt.Printf("ADDED unit \"%s\" for ingredient \"%s\" for cake  \"%s\"\n", ing.Unit, ing.Name, other.Name)
+			} else if ing.Unit == "" {
+				fmt.Printf("REMOVED unit \"%s\" for ingredient \"%s\" for cake  \"%s\"\n", ings[keyToLow].Unit, ings[keyToLow].Name, c.Name)
+			} else {
+				fmt.Printf("CHANGED unit for ingredient \"%s\" for cake  \"%s\" - \"%s\" instead of \"%s\"\n",
+					ings[keyToLow].Name, c.Name, ing.Unit, ings[keyToLow].Unit)
+			}
+		}
+	}
+	// Доп проход для вывода удаленных ингредиентов
+	for _, ing := range ings {
+		if _, ok := commonIngs[ing.Name]; !ok {
+			fmt.Printf("REMOVED ingredient \"%s\" for cake  \"%s\"\n", ing.Name, other.Name)
+		}
+	}
 }
