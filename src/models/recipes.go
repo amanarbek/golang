824b3dfce5680182ba9af8336f8dd0d61893f998
Manarbek Abrashimov <avmanarbek@gmail.com>
2025-07-12 22:57:25 +0500

    Добавил метод для сравнения рецепта пирога без учета ингредиентов

--- src/models/recipes.go
+++ src/models/recipes.go
@@ -1,6 +1,9 @@
 package models
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"fmt"
+)
 
 type Ingredient struct {
 	XMLName xml.Name `json:"-" xml:"item"`
@@ -18,4 +21,42 @@ type Cake struct {
 type Recipes struct {
 	XMLName xml.Name `json:"-" xml:"recipes"`
 	Cakes   []Cake   `json:"cake" xml:"cake"`
+}
+
+func (r Recipes) CompareRecipes(other Recipes) {
+	orgRes := make(map[string]Cake)
+	commonRecipes := make(map[string]Cake)
+
+	for _, cake := range r.Cakes {
+		orgRes[cake.Name] = cake
+	}
+
+	// Найти добавленные и удалённые торты
+	for _, cake := range other.Cakes {
+		if _, ok := orgRes[cake.Name]; !ok {
+			fmt.Printf("ADDED cake %q\n", cake.Name)
+		} else {
+			commonRecipes[cake.Name] = cake
+		}
+	}
+
+	for _, cake := range orgRes {
+		if _, ok := commonRecipes[cake.Name]; !ok {
+			fmt.Printf("REMOVED cake %q\n", cake.Name)
+		}
+	}
+
+	// Найти изменения в общих тортах
+	for name, cake := range commonRecipes {
+		if cake.Time != orgRes[name].Time {
+			fmt.Printf("CHANGED cooking time for cake \"%s\" - \"%s\" instead of \"%s\"\n", name, cake.Time, orgRes[name].Time)
+		}
+
+		// И сравнение ингредиентов
+		orgRes[name].compareIngredients(cake)
+	}
+}
+
+func (c Cake) compareIngredients(other Cake) {
+
 }
